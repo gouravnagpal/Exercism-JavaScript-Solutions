b179ff67070d28bfbaed89a49693ea123589fb86
unknown <achievers.app.community@gmail.com>
2022-12-21 21:34:21 +0530

    Updates

---
+++ 2-lasagna.js
@@ -0,0 +1,12 @@
+const PREPARATION_MINUTES_PER_LAYER = 2;
+export const EXPECTED_MINUTES_IN_OVEN = 40;
+
+export function remainingMinutesInOven(actualMinutesInOven) {
+    return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
+}
+export function preparationTimeInMinutes(numberOfLayers) {
+    return PREPARATION_MINUTES_PER_LAYER * numberOfLayers;
+}
+export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
+    return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+}
