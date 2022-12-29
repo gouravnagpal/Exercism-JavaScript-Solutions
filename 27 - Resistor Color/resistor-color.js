177d66db4dadd282f50f68b527d1eaea623b0c06
unknown <achievers.app.community@gmail.com>
2022-12-29 16:52:15 +0530

    Updates

---
+++ 27 - Resistor Color/resistor-color.js
@@ -0,0 +1,18 @@
+export const codes = {
+    'black' : 0,
+    'brown' : 1,
+    'red' : 2,
+    'orange' : 3,
+    'yellow' : 4,
+    'green' : 5,
+    'blue' : 6,
+    'violet' : 7,
+    'grey' : 8,
+    'white' : 9,
+}
+
+export const colorCode = (color) => {
+    return color === COLORS ? COLORS : codes[color];
+};
+
+export const COLORS = Object.keys(codes);
