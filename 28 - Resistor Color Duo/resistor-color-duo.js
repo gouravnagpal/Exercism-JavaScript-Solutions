a51b4b235d018499d9e7da65440112746d931451
unknown <achievers.app.community@gmail.com>
2022-12-29 17:00:33 +0530

    Updates

---
+++ 28 - Resistor Color Duo/resistor-color-duo.js
@@ -0,0 +1,16 @@
+export const codes = {
+    'black' : 0,
+    'brown' : 1,
+    'red' : 2,
+    'orange' : 3,
+    'yellow' : 4,
+    'green' : 5,
+    'blue' : 6,
+    'violet' : 7,
+    'grey' : 8,
+    'white' : 9,
+}
+
+export const decodedValue = ([color1, color2]) => {
+    return parseInt(`${codes[color1]}${codes[color2]}`);
+};
