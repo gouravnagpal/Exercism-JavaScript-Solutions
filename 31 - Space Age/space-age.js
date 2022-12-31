e684644218064bc6d954a611540c9164bb4edf04
unknown <achievers.app.community@gmail.com>
2022-12-31 20:37:38 +0530

    Updates

---
+++ 31 - Space Age/space-age.js
@@ -0,0 +1,13 @@
+export const age = (Planet, Age) => {
+    const combine = {
+        'mercury': 0.2408467,
+        'venus': 0.61519726,
+        'earth': 1.0,
+        'mars': 1.8808158,
+        'jupiter': 11.862615,
+        'saturn': 29.447498,
+        'uranus': 84.016846,
+        'neptune': 164.79132,
+    }
+    return Number((Age / 31557600 / combine[Planet]).toFixed(2));
+};
