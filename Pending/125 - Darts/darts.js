37cf5217e55514dc9188e3643f5cf1c0303744b8
gourav-nagpal21 <snagpal676@gmail.com>
2023-01-07 14:54:17 +0530

    Updates

---
+++ Pending/125 - Darts/darts.js
@@ -0,0 +1,8 @@
+export function score(x, y) {
+    const distance = Math.sqrt(x*x + y*y);
+
+    if (distance <= 1) return 10;
+    if (distance <= 5) return 5;
+    if (distance <= 10) return 1;
+    return 0;
+}
