8cac0277484a79190c947ddf989c1b4a299432b6
Gourav Nagpal <snagpal676@gmail.com>
2023-02-03 21:38:06 +0530

    Updates

---
+++ 109 - Flatten Array/flatten-array.js
@@ -0,0 +1,19 @@
+export const flatten = (a) => {
+    let result = new Array();
+    if(a.length < 1) return result;
+  
+    function decodeArray(arr){
+      for(let i = 0; i < arr.length; i++){
+        if(arr[i] !== null && typeof arr[i] == 'object'){
+            decodeArray(arr[i]);
+        }
+        else{
+            if(arr[i] !== null) result.push(arr[i]);
+        }
+      }
+    }
+  
+    decodeArray(a);
+  
+    return result;
+  };
